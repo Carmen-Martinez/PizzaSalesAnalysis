a187e828f3c40dcfb71fd7b4314cc45f074a6a18
Carmen <carmenm.data@gmail.com>
2024-01-17 16:08:52 -0500

    updated script

--- Data Cleaning & EDA.sql
+++ Data Cleaning & EDA.sql
@@ -99,19 +99,6 @@ SELECT COUNT(DISTINCT(order_id))
 FROM pizza_sales;
 -- [21350 unique orders placed]
 
-# What is the total revenue generated over all operational days?
-SELECT ROUND(SUM(total_price),1) AS total_rev, MAX(order_date) - MIN(order_date) AS number_of_days
-FROM pizza_sales;
--- [$817,860 total revenue in 8 days]
-
-# Busiest time for the pizza store
-SELECT order_time, COUNT(order_time) as order_count
-FROM pizza_sales
-GROUP BY order_time
-ORDER BY order_count DESC
-LIMIT 1000;
--- [Most pizzas are ordered between 11am-1pm]
-
 # TOP 10 pizzas ordered
 SELECT pizza_name, COUNT(pizza_name) AS num_pizza_orders
 FROM pizza_sales
@@ -152,24 +139,35 @@ FROM (
 -- [The TOP 10 Pizza's total revenue is 2x the BOTTOM 10 Pizza's total revenue]
 
 # most favorite ingredient
-SELECT DISTINCT(1st_main_ingredient), COUNT(1st_main_ingredient) AS fav_1st_main_ingredient -- , 2nd_main_ingredient, 3rd_main_ingredient
+SELECT DISTINCT(main_topping), COUNT(main_topping) AS fav_main_topping -- , 2nd_main_ingredient, 3rd_main_ingredient
 FROM pizza_sales
-GROUP BY 1st_main_ingredient
-ORDER BY fav_1st_main_ingredient DESC;
+GROUP BY main_topping
+ORDER BY fav_main_topping DESC;
 -- [Chicken is the most favorite main ingredient on pizza]
 
 # 2nd most favorite ingredient
-SELECT DISTINCT(2nd_main_ingredient), COUNT(2nd_main_ingredient) AS fav_2nd_main_ingredient -- , 2nd_main_ingredient, 3rd_main_ingredient
+SELECT DISTINCT(2nd_main_topping), COUNT(2nd_main_topping) AS fav_2nd_main_topping -- , 2nd_main_ingredient, 3rd_main_ingredient
 FROM pizza_sales
-GROUP BY 2nd_main_ingredient
-ORDER BY fav_2nd_main_ingredient DESC;
--- [mushroom is the 2nd most favoite ingredient, if we assume tomato is not a topping but part of all pizzas]
+GROUP BY 2nd_main_topping
+HAVING 2nd_main_topping <> 'tomatoes'
+ORDER BY fav_2nd_main_topping DESC;
+-- [mushroom is the 2nd most favoite ingredient, if we remove tomatoes and mozzarella cheese]
 
 # 3rd most favorite ingredient
-SELECT DISTINCT(3rd_main_ingredient), COUNT(3rd_main_ingredient) AS fav_3rd_main_ingredient -- , 2nd_main_ingredient, 3rd_main_ingredient
+SELECT DISTINCT(3rd_main_topping), COUNT(3rd_main_topping) AS fav_3rd_main_topping -- , 2nd_main_ingredient, 3rd_main_ingredient
 FROM pizza_sales
-GROUP BY 3rd_main_ingredient
-ORDER BY fav_3rd_main_ingredient DESC
+GROUP BY 3rd_main_topping
+HAVING 3rd_main_topping <> 'tomatoes' AND 3rd_main_topping <> 'mozzarella cheese'
+ORDER BY fav_3rd_main_topping DESC;
+-- [red peppers is the 2nd most favoite ingredient, if we remove tomatoes and mozzarella cheese]
+
+# What is the total revenue generated over all operational days?
+SELECT DATEDIFF(MAX(order_date), MIN(order_date)) + 1, ROUND(SUM(total_price),2) AS total_rev
+FROM pizza_sales;
+-- [$817,860 total revenue in 1 year]
+
+
+
 
 
 
